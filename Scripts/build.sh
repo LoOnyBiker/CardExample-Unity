a71c8021f3baeb45d12092e05d203d7eff3b3a71
LoOnyBiker <loonybiker@yandex.ru>
2017-07-28 14:40:36 +0300

    Attempt to enable Win in build script

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -2,15 +2,15 @@
 
 project="TravisCI"
 
-#echo "Attempting to build $project for Windows"
-#/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-#  -batchmode \
-#  -nographics \
-#  -silent-crashes \
-#  -logFile $(pwd)/unity.log \
-#  -projectPath $(pwd) \
-#  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
-#  -quit
+echo "Attempting to build $project for Windows"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/unity.log \
+  -projectPath $(pwd) \
+  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
+  -quit
 
 echo "Attempting to build $project for OS X"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
