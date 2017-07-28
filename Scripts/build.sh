ba682e99d46cf090230c1abdc1462b4c646512c8
LoOnyBiker <loonybiker@yandex.ru>
2017-07-28 14:08:39 +0300

    Forgot to add project to build

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -12,15 +12,15 @@ project="TravisCI"
 #  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
 #  -quit
 
-#echo "Attempting to build $project for OS X"
-#/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-#  -batchmode \
-#  -nographics \
-#  -silent-crashes \
-#  -logFile $(pwd)/unity.log \
-#  -projectPath $(pwd) \
-#  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
-#  -quit
+echo "Attempting to build $project for OS X"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/unity.log \
+  -projectPath $(pwd) \
+  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
+  -quit
 
 #echo "Attempting to build $project for Linux"
 #/Applications/Unity/Unity.app/Contents/MacOS/Unity \
