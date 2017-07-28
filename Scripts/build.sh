5556a309f448387b0958a473b040efc591630f8a
LoOnyBiker <loonybiker@yandex.ru>
2017-07-28 14:27:51 +0300

    Remove zip

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -38,5 +38,5 @@ cat $(pwd)/unity.log
 
 #echo 'Attempting to zip builds'
 #zip -r $(pwd)/Build/linux.zip $(pwd)/Build/linux/
-zip -r $(pwd)/Build/mac.zip $(pwd)/Build/osx/
+#zip -r $(pwd)/Build/mac.zip $(pwd)/Build/osx/
 #zip -r $(pwd)/Build/windows.zip $(pwd)/Build/windows/
