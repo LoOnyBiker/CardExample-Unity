ba7ba73b2047a7ee2c0c30e249bed36041acc527
LoOnyBiker <loonybiker@yandex.ru>
2017-07-28 14:14:28 +0300

    Fix error of pasting into shell

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -9,7 +9,7 @@ download() {
   url="$BASE_URL/$HASH/$package"
 
   echo "Downloading from $url: "
-  curl -o `basename "$package"` "$url" —-retry 5
+  curl -o `basename "$package"` "$url" --retry 5
 }
 
 install() {
